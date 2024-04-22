196d7bcaa4c5e24e0babab90cc118b1fcd8a7c02
Michał Wróblewski <272488@student.pwr.edu.pl>
2024-04-22 18:30:15 +0200

    Restructuring

--- main.cpp
+++ main.cpp
@@ -1,48 +1,8 @@
 ﻿#include <iostream>
 #include <chrono>
 #include <vector>
-#include "array.h"
-#include "arrayMalloc.h"
+#include "measureFunctions.cpp"
 using namespace std;
-
-auto timeMeasureMallocArray(int value) {
-	auto start = chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < 1000000; i++) {
-		ArrayMalloc arr;
-		for (size_t j = 0; j < value; j++) {
-			arr.push(j);
-		}
-	}
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-	return duration / 1000000;
-}
-
-auto timeMeasureArray(int value) {
-	auto start = chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < 1000000; i++) {
-		Array arr;
-		for (size_t j = 0; j < value; j++) {
-			arr.push(j);
-		}
-	}
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-	return duration / 1000000;
-}
-
-auto timeMeasureVector(int value) {
-	auto start = chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < 1000000; i++) {
-		vector<int> arr;
-		for (size_t j = 0; j < value; j++) {
-			arr.push_back(j);
-		}
-	}
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-	return duration / 1000000;
-}
 
 int main()
 {
@@ -50,13 +10,13 @@ int main()
 	int highest = 0;
 
 	for (int i = 0; i < 100; i++) {
-		auto malArrayTime = timeMeasureMallocArray(i);
-		auto arrayTime = timeMeasureArray(i);
+		auto malArrayTime = pushTimeMeasureMallocArray(i);
+		auto arrayTime = pushTimeMeasureArray(i);
 		if (malArrayTime < arrayTime) {
 			highest = i;
 			cout << "I = " << highest << endl;
-			cout << "Time taken by ArrayMalloc: " << timeMeasureMallocArray(i) << " ns" << endl;
-			cout << "Time taken by Array: " << timeMeasureArray(i) << " ns" << endl;
+			cout << "Time taken by ArrayMalloc: " << pushTimeMeasureMallocArray(i) << " ns" << endl;
+			cout << "Time taken by Array: " << pushTimeMeasureArray(i) << " ns" << endl;
 			cout << endl;
 			count = 0;
 		}
